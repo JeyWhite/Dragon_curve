b999c70387e6beca4456eeccba4edc84de997500
Jay White <evg.b42@ya.ru>
2020-11-07 03:41:20 +0700

    now app draw 4 curves. It look like a flower.

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import defs
 
 
 Current_Rules = {"X": "X+YF+", "Y": "-FX-Y"}
+colors = {"blue","red","green","yellow"}
 print("Вбей количество итераций:")
 iterations = int(input())
 res = defs.processing("FX", Current_Rules, iterations)
 print(res) # for debugging
 turtle.speed(0)
 turtle.bgcolor("black")
-turtle.pencolor("blue")
 turtle.ht()
 turtle.pensize(2)
-# defs.
-# defs.square_draw(res, 90, int(20 / iterations))
+for curve in colors:
+    turtle.pu()
+    turtle.setpos(0,0)
+    turtle.right(90)
+    turtle.pencolor(curve)
+    turtle.pd()
+    # расскоментируй стиль отрисовки
+    # defs.curved_draw(res, 90, int(20 / iterations))
+    defs.square_draw(res, 90, int(80 / iterations))
 turtle.done()
