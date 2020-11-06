6b66be0170b256d966350f7a1f6b77a8e24768fc
Jay White <evg.b42@ya.ru>
2020-11-07 04:15:57 +0700

    pretty colors

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import turtle
 import defs
 
-
 Current_Rules = {"X": "X+YF+", "Y": "-FX-Y"}
-colors = {"blue","red","green","yellow"}
+colors = {"blue", "red", "green", "gold"}
 print("Вбей количество итераций:")
 iterations = int(input())
 res = defs.processing("FX", Current_Rules, iterations)
-print(res) # for debugging
+print(res)  # for debugging
 turtle.speed(0)
 turtle.bgcolor("black")
 turtle.ht()
-turtle.pensize(2)
-for curve in colors:
+turtle.pensize(1)
+for color in colors:
     turtle.pu()
-    turtle.setpos(0,0)
+    turtle.setpos(0, 0)
     turtle.right(90)
-    turtle.pencolor(curve)
+    turtle.pencolor(color)
     turtle.pd()
     # расскоментируй стиль отрисовки
     # defs.curved_draw(res, 90, int(20 / iterations))
